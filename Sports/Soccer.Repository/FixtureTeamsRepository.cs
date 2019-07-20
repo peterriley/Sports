efaea8c0440c508005d491e7dcf3168011cca065
Peter Riley <peter.riley@marketingyard.com>
2019-07-20 13:46:10 +0100

    Update FixtureTeamsRepository.cs

--- Sports/Soccer.Repository/FixtureTeamsRepository.cs
+++ Sports/Soccer.Repository/FixtureTeamsRepository.cs
@@ -9,6 +9,29 @@ namespace Soccer.Repository
 {
     public class FixtureTeamsRepository
     {
+        private List<Team> PremierTeamList = new List<Team>() {
+                new Team(){TeamId = 1, Name = "Arsenal"},
+                new Team(){TeamId = 2, Name = "Aston Villa"},
+                new Team(){TeamId = 3, Name = "Bournemouth"},
+                new Team(){TeamId = 4, Name = "Brighton"},
+                new Team(){TeamId = 5, Name = "Burnley"},
+                new Team(){TeamId = 6, Name = "Chelsea"},
+                new Team(){TeamId = 7, Name = "Crystal Palace"},
+                new Team(){TeamId = 8, Name = "Everton"},
+                new Team(){TeamId = 9, Name = "Leicester"},
+                new Team(){TeamId = 10, Name = "Liverpool"},
+                new Team(){TeamId = 11, Name = "Manchester City"},
+                new Team(){TeamId = 12, Name = "Manchester United"},
+                new Team(){TeamId = 13, Name = "Newcastle"},
+                new Team(){TeamId = 14, Name = "Norwich"},
+                new Team(){TeamId = 15, Name = "Sheffield United"},
+                new Team(){TeamId = 16, Name = "Southampton"},
+                new Team(){TeamId = 17, Name = "Tottenham"},
+                new Team(){TeamId = 18, Name = "Watford"},
+                new Team(){TeamId = 19, Name = "West Ham"},
+                new Team(){TeamId = 20, Name = "Wolves"}
+            };
+
         public FixtureTeamsRepository()
         {
             playerRepository = new PlayerRepository();
@@ -44,58 +67,14 @@ namespace Soccer.Repository
         public FixtureTeams Retrieve()
         {
             // Code that retrieves all of the fixture teams
-            List<Team> homeTeamList = new List<Team>() {
-                new Team(){TeamId = 1, Name = "Arsenal"},
-                new Team(){TeamId = 2, Name = "Aston Villa"},
-                new Team(){TeamId = 3, Name = "Bournemouth"},
-                new Team(){TeamId = 4, Name = "Brighton"},
-                new Team(){TeamId = 5, Name = "Burnley"},
-                new Team(){TeamId = 6, Name = "Chelsea"},
-                new Team(){TeamId = 7, Name = "Crystal Palace"},
-                new Team(){TeamId = 8, Name = "Everton"},
-                new Team(){TeamId = 9, Name = "Leicester"},
-                new Team(){TeamId = 10, Name = "Liverpool"},
-                new Team(){TeamId = 11, Name = "Manchester City"},
-                new Team(){TeamId = 12, Name = "Manchester United"},
-                new Team(){TeamId = 13, Name = "Newcastle"},
-                new Team(){TeamId = 14, Name = "Norwich"},
-                new Team(){TeamId = 15, Name = "Sheffield United"},
-                new Team(){TeamId = 16, Name = "Southampton"},
-                new Team(){TeamId = 17, Name = "Tottenham"},
-                new Team(){TeamId = 18, Name = "Watford"},
-                new Team(){TeamId = 19, Name = "West Ham"},
-                new Team(){TeamId = 20, Name = "Wolves"}
-            };
-            List<string> homeTeamName = homeTeamList.Select(ht => ht.Name).ToList();
-            List<Team> awayTeamList = new List<Team>() {
-                new Team(){TeamId = 1, Name = "Arsenal"},
-                new Team(){TeamId = 2, Name = "Aston Villa"},
-                new Team(){TeamId = 3, Name = "Bournemouth"},
-                new Team(){TeamId = 4, Name = "Brighton"},
-                new Team(){TeamId = 5, Name = "Burnley"},
-                new Team(){TeamId = 6, Name = "Chelsea"},
-                new Team(){TeamId = 7, Name = "Crystal Palace"},
-                new Team(){TeamId = 8, Name = "Everton"},
-                new Team(){TeamId = 9, Name = "Leicester"},
-                new Team(){TeamId = 10, Name = "Liverpool"},
-                new Team(){TeamId = 11, Name = "Manchester City"},
-                new Team(){TeamId = 12, Name = "Manchester United"},
-                new Team(){TeamId = 13, Name = "Newcastle"},
-                new Team(){TeamId = 14, Name = "Norwich"},
-                new Team(){TeamId = 15, Name = "Sheffield United"},
-                new Team(){TeamId = 16, Name = "Southampton"},
-                new Team(){TeamId = 17, Name = "Tottenham"},
-                new Team(){TeamId = 18, Name = "Watford"},
-                new Team(){TeamId = 19, Name = "West Ham"},
-                new Team(){TeamId = 20, Name = "Wolves"}
-            };
-            List<string> awayTeamName = awayTeamList.Select(ht => ht.Name).ToList();
+            List<string> homeTeamName = PremierTeamList.Select(ht => ht.Name).ToList();
+            List<string> awayTeamName = PremierTeamList.Select(ht => ht.Name).ToList();
 
             FixtureTeams fixtureTeams = new FixtureTeams()
             {
                 FixtureTeamsId = 1,
-                HomeTeamList = homeTeamList,
-                AwayTeamList = awayTeamList
+                HomeTeamList = PremierTeamList,
+                AwayTeamList = PremierTeamList
             };
             
 
